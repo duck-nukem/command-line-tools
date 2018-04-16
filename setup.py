1bdcc7c7e7fb78c1da546a17e0fdca9f06c2a114
Alex Szabó <kreatemore@gmail.com>
2018-04-16 15:39:38 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Distutils',
+    version='1.0',
+    description='Small library for CLIs',
+    author='Alex Szabo',
+    author_email='kreatemore@gmail.com',
+)
